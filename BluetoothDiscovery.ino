c95f2ca6fa710071730c2a01c2b87ff39b97bbba
Philip Giacalone <philip_giacalone@yahoo.com>
2023-09-18 14:35:07 -0700

    Added code to handle more event types in app_gap_callback(). The code is discovering multiple devices and correctly reporting their logical names and BDA (MAC) addresses.

--- BluetoothDiscovery.ino
+++ BluetoothDiscovery.ino
@@ -31,16 +31,17 @@
 #define TARGET_DEVICE_NAME "TargetDevice"
 bool device_found = false;
 
+bool isDiscoveryComplete = false;
+
 // a lookup table for the Bluetooth earbuds we want to discover, pair with and connect to.
 std::map<std::string, std::string> deviceNames = {
     // {"74:74:46:ED:07:6B", "Pixel Buds A Series"},
     {"58:FC:C6:6C:0A:03", "TOZO Home"},
     {"54:B7:E5:8C:07:71", "TOZO Mazda"}
-    //REMINDER: no trailing comma!
+    //REMINDER: no trailing comma
 };
 
 std::map<std::string, esp_bd_addr_t> discoveredDevices;
-
 
 //Bluetooth Device Address (with is the MAC (Media Access Control))
 esp_bd_addr_t target_device_bda;
@@ -127,14 +128,6 @@ void Initialize_Stack() {
     esp_bt_dev_set_device_name("ESP32_Device");
 }
 
-void pair_with_device(esp_bt_gap_cb_param_t *param) {
-    Serial.println("Pairing with target device...");
-    
-    esp_bt_pin_code_t pin_code = {'1', '2', '3', '4'};
-    esp_bt_gap_set_pin(ESP_BT_PIN_TYPE_FIXED, 4, pin_code);
-    esp_bt_gap_pin_reply(param->disc_res.bda, true, 4, pin_code);
-}
-
 // connect to an A2DP device
 void connect_a2dp(esp_bd_addr_t remote_bda) {
     esp_a2d_sink_connect(remote_bda);
@@ -149,7 +142,7 @@ void Start_Discovery() {
 }
 
 /*  
- * GAP stands for "Generic Access Profile." It's one of the core protocols 
+ * GAP stands for "Generic Access Profile." It's one of the core bluetooth protocols. 
  * GAP Handles:
  *  1) Discovery: GAP manages the discovery process, i.e., how devices advertise themselves and how they scan for other devices. This includes setting the device to be discoverable or non-discoverable, setting the device name, and defining the scan modes.
  *  2) Connection: Once two devices have discovered each other, GAP also handles how they establish a connection. This includes security features such as pairing, bonding, and the use of passkeys.
@@ -170,52 +163,102 @@ void app_gap_callback(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param)
                 // Store the BDA and its logical name in the discoveredDevices map
                 memcpy(discoveredDevices[bda_str], param->disc_res.bda, sizeof(esp_bd_addr_t));
                 
-                // Optional: Set pin if you want to pair immediately after discovery.
-                // esp_bt_pin_code_t pin_code = {'1', '2', '3', '4'};
-                // esp_bt_gap_set_pin(ESP_BT_PIN_TYPE_FIXED, 4, pin_code);
-                // esp_bt_gap_pin_reply(param->disc_res.bda, true, 4, pin_code);
             }
 
             // If we've discovered all devices in our map, stop discovery
             if (discoveredDevices.size() == deviceNames.size()) {
                 Serial.println("Found all target devices. Stopping discovery...");
                 esp_bt_gap_cancel_discovery();
+                isDiscoveryComplete = true;
             }
+
             break;
         }
+
+        case ESP_BT_GAP_AUTH_CMPL_EVT: {
+            if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+                Serial.printf("Authentication success: %s\n", param->auth_cmpl.device_name);
+                Serial.printf("BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                              param->auth_cmpl.bda[0], param->auth_cmpl.bda[1], param->auth_cmpl.bda[2],
+                              param->auth_cmpl.bda[3], param->auth_cmpl.bda[4], param->auth_cmpl.bda[5]);
+            } else {
+                Serial.printf("Authentication failed, status: %d\n", param->auth_cmpl.stat);
+                Serial.printf("BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                              param->auth_cmpl.bda[0], param->auth_cmpl.bda[1], param->auth_cmpl.bda[2],
+                              param->auth_cmpl.bda[3], param->auth_cmpl.bda[4], param->auth_cmpl.bda[5]);
+            }
+            break;
+        }        
+
+        case ESP_BT_GAP_DISC_STATE_CHANGED_EVT:
+            Serial.println("Discovery state changed.");
+            break;
+
+        case ESP_BT_GAP_RMT_SRVCS_EVT:
+            Serial.printf("Remote services for BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                          param->rmt_srvcs.bda[0], param->rmt_srvcs.bda[1], param->rmt_srvcs.bda[2],
+                          param->rmt_srvcs.bda[3], param->rmt_srvcs.bda[4], param->rmt_srvcs.bda[5]);
+            break;
+
+        case ESP_BT_GAP_RMT_SRVC_REC_EVT:
+            Serial.println("Received remote service record.");
+            break;
+
+        case ESP_BT_GAP_PIN_REQ_EVT:
+            Serial.printf("PIN code request for BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                          param->pin_req.bda[0], param->pin_req.bda[1], param->pin_req.bda[2],
+                          param->pin_req.bda[3], param->pin_req.bda[4], param->pin_req.bda[5]);
+            break;
+
+        case ESP_BT_GAP_CFM_REQ_EVT:
+            Serial.printf("Confirm request for passkey: %d, for BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                          param->cfm_req.num_val,
+                          param->cfm_req.bda[0], param->cfm_req.bda[1], param->cfm_req.bda[2],
+                          param->cfm_req.bda[3], param->cfm_req.bda[4], param->cfm_req.bda[5]);
+            break;
+
+        case ESP_BT_GAP_KEY_NOTIF_EVT:
+            Serial.printf("Passkey notification: %d, for BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                          param->key_notif.passkey,
+                          param->key_notif.bda[0], param->key_notif.bda[1], param->key_notif.bda[2],
+                          param->key_notif.bda[3], param->key_notif.bda[4], param->key_notif.bda[5]);
+            break;
+
+        case ESP_BT_GAP_KEY_REQ_EVT:
+            Serial.printf("Passkey request for BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                          param->key_req.bda[0], param->key_req.bda[1], param->key_req.bda[2],
+                          param->key_req.bda[3], param->key_req.bda[4], param->key_req.bda[5]);
+            break;
+
+        case ESP_BT_GAP_READ_RSSI_DELTA_EVT:
+            /* Commented out due to compilation issue
+            Serial.printf("RSSI delta for BDA: %02X:%02X:%02X:%02X:%02X:%02X, RSSI: %d\n",
+                          param->read_rssi_delta.bda[0], param->read_rssi_delta.bda[1], param->read_rssi_delta.bda[2],
+                          param->read_rssi_delta.bda[3], param->read_rssi_delta.bda[4], param->read_rssi_delta.bda[5],
+                          param->read_rssi_delta.rssi);
+            */
+            Serial.printf("RSSI delta event for BDA: %02X:%02X:%02X:%02X:%02X:%02X\n",
+                          param->read_rssi_delta.bda[0], param->read_rssi_delta.bda[1], param->read_rssi_delta.bda[2],
+                          param->read_rssi_delta.bda[3], param->read_rssi_delta.bda[4], param->read_rssi_delta.bda[5]);
+            break;
+
+        // case ESP_BT_GAP_LINK_STATUS_EVT:
+        //     Serial.printf("Link status changed for BDA: %02X:%02X:%02X:%02X:%02X:%02X, status: %d\n",
+        //                   param->link_status.bda[0], param->link_status.bda[1], param->link_status.bda[2],
+                         
+        case ESP_BT_GAP_SET_AFH_CHANNELS_EVT:
+            // Handle the set AFH channels event here
+            Serial.println("Set AFH channels event triggered.");
+            break;
+
+        case ESP_BT_GAP_CONFIG_EIR_DATA_EVT:
+            // Handle the config EIR data event here
+            Serial.println("Config EIR data event triggered.");
+            break;
+
         // ... (other cases remain unchanged)
     }
 }
-
-
-// void app_gap_callback(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *param) {
-//     switch (event) {
-//         case ESP_BT_GAP_DISC_RES_EVT: {
-//             char bda_str[18];
-//             sprintf(bda_str, "%02X:%02X:%02X:%02X:%02X:%02X", 
-//                 param->disc_res.bda[0], param->disc_res.bda[1], param->disc_res.bda[2], 
-//                 param->disc_res.bda[3], param->disc_res.bda[4], param->disc_res.bda[5]);
-
-//             // Checking if the discovered BDA exists in our predefined set
-//             auto it = deviceNames.find(bda_str);
-//             if (it != deviceNames.end()) {
-//                 Serial.printf("Found target device: %s (BDA: %s)\n", it->second.c_str(), bda_str);
-                
-//                 // Storing the BDA of the discovered target device
-//                 memcpy(target_device_bda, param->disc_res.bda, sizeof(esp_bd_addr_t));
-
-//                 esp_bt_gap_cancel_discovery();
-
-//                 esp_bt_pin_code_t pin_code = {'1', '2', '3', '4'};
-//                 esp_bt_gap_set_pin(ESP_BT_PIN_TYPE_FIXED, 4, pin_code);
-//                 esp_bt_gap_pin_reply(param->disc_res.bda, true, 4, pin_code);
-//             }
-//             break;
-//         }
-//         // ... (other cases remain unchanged)
-//     }
-// }
-
 
 void a2dp_callback(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *a2d) {
     switch (event) {
@@ -230,10 +273,45 @@ void a2dp_callback(esp_a2d_cb_event_t event, esp_a2d_cb_param_t *a2d) {
     }
 }
 
+void pair_with_device(const uint8_t* bda, const std::string& deviceName) {
+    // Declare the pin_code here
+    esp_bt_pin_code_t pin_code = {'1', '2', '3', '4'};
+
+    // Your pairing code here...
+    Serial.printf("Attempting to pair with device: %s (BDA: %02X:%02X:%02X:%02X:%02X:%02X)\n", 
+                  deviceName.c_str(), bda[0], bda[1], bda[2], bda[3], bda[4], bda[5]);
+    
+    // Set the PIN
+    esp_bt_gap_set_pin(ESP_BT_PIN_TYPE_FIXED, 4, pin_code);
+    
+    // Initiate pairing
+    esp_err_t ret = esp_bt_gap_pin_reply(const_cast<uint8_t*>(bda), true, 4, pin_code);
+    if (ret != ESP_OK) {
+        Serial.printf("Error in pairing with BDA: %02X:%02X:%02X:%02X:%02X:%02X. Error code: %s\n", 
+                      bda[0], bda[1], bda[2], bda[3], bda[4], bda[5], esp_err_to_name(ret));
+    }
+}
+
+void pair_with_all_discovered_devices() {
+    for (auto& pair : discoveredDevices) {
+        pair_with_device(const_cast<uint8_t*>(pair.second), deviceNames[pair.first]);
+        // pair_with_device(const_cast<uint8_t*>(pair.second), pair.first);
+        vTaskDelay(pdMS_TO_TICKS(2000)); // 2-second delay for clarity and to avoid too rapid consecutive actions
+    }
+}
+
 void app_main() {
     Initialize_Stack();
-    Serial.println("Stack Initialization Complete");
     Start_Discovery();
+
+    while(1) { // Forever loop
+        if (isDiscoveryComplete) {
+            Serial.println("Discovery complete. Initiating pairing...");
+            pair_with_all_discovered_devices();
+            isDiscoveryComplete = false;  // Reset the flag to avoid re-pairing in this example
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));  // Delay for half a second before checking again
+    }
 }
 
 void setup() {
